7b470d45778efae28923de3d70570e7dffc6f4ab
JG Morse <dealerv2@outlook.com>
2023-03-05 22:32:08 -0800

    recompiled dealerv2 object with no DBG code

--- include/dbgprt_macros.h
+++ include/dbgprt_macros.h
@@ -2,7 +2,7 @@
 #ifndef DBGPRT_MACROS_H
 #define DBGPRT_MACROS_H
 /* this next line should come from the environment. */
- #define JGMDBG 1   /* for testing  */
+// #define JGMDBG 1   // for testing  
 #ifdef JGMDBG
   #define DBGPRINT(...) fprintf(stderr, __VA_ARGS__)
   #define DBGLOC(fmt,...) fprintf(stderr, "%s:%d " fmt, __FILE__,__LINE__,## __VA_ARGS__)
